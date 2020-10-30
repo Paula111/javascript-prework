5658a35d9d77ad94198091e1bfa1fc37bcec6963
Paulina Babinczuk <paulina.babinczuk@gmail.com>
2020-10-30 11:28:29 +0100

    displayResult_function

--- js/script.js
+++ js/script.js
@@ -17,34 +17,20 @@ const getMoveName = function (argMoveId) {
     return 'kamień';
   }
 }
-function displayResult(argPlayerMove, argComputerMove) { 
-
+const displayResult = function (argPlayerMove, argComputerMove) { 
   console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove); 
-
   if (argPlayerMove == 'papier' && argComputerMove == 'kamień') { 
-
     printMessage('Wygrywasz!'); 
-
   } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') { 
-
     printMessage('Wygrywasz!'); 
-
   } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') { 
-
     printMessage('Wygrywasz!'); 
-
   } else if (argPlayerMove == argComputerMove) { 
-
     printMessage('Remis'); 
-
   } else { 
-
     printMessage('Przegrywasz :('); 
-
   } 
-
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove); 
-
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove); 
 } 
 
 playerMove = argButtonName; 
